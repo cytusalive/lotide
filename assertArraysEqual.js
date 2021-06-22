34eeea6e1755da52e60d3650d41fa2024bda580e
cytusalive <cytusomega@gmail.com>
2021-06-22 23:52:12 +0000

    assert for arrays

---
+++ assertArraysEqual.js
@@ -0,0 +1,24 @@
+const eqArrays = function(ar1, ar2) {
+  if (ar1.length == ar2.length) {
+    for (let i = 0; i < ar1.length; i++) {
+      if (ar1[i] !== ar2[i]) {
+        return false;
+      }
+    }
+  } else {
+    return false;
+  }
+  return true;
+}
+
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log(`✅✅✅ Assertion Passed: [${array1}] === [${array2}]`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: [${array1}] !== [${array2}]`);
+  }
+};
+
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
+assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]);
+assertArraysEqual(["1", "2", "3"], ["1", "2", 3]);
