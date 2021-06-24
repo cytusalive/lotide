20e96cd2270f5392bb769ff0e0122ca3922db7bc
cytusalive <cytusomega@gmail.com>
2021-06-24 03:13:07 +0000

    count the number of each letter in a sentence

---
+++ countLetters.js
@@ -0,0 +1,23 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function(sentence) {
+  let finalCount = {};
+  for (letter of sentence) {
+    if (letter !== ' ') {
+      if (finalCount[letter]) {
+        finalCount[letter] += 1;
+      } else {
+        finalCount[letter] = 1;
+      }
+    }
+  }
+  return finalCount;
+}
+
+console.log(countLetters("lighthouse in the house"));
